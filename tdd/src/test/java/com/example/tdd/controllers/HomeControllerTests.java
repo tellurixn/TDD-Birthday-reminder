174d01a035941ac19c3dcab6ea3b1f861079b160
tellurixn <victor.babaryckin@yandex.ru>
2024-03-31 16:10:59 +0700

    RED: Test for GET-method from HomeController

---
+++ tdd/src/test/java/com/example/tdd/controllers/HomeControllerTests.java
@@ -0,0 +1,29 @@
+package com.example.tdd.controllers;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    /*Тест, выполняющий HTTP-запрос GET с корневым путем / и затем убеждающийся, что
+    контроллер выбрал имя представления home, а сама страница содержит фразу «Друзья».*/
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        containsString("Друзья")));
+    }
+}
