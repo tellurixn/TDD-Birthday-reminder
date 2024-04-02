ff2e47e1efd2c5065c953ec742f7def5883550b7
tellurixn <victor.babaryckin@yandex.ru>
2024-04-02 19:28:28 +0700

    RED: New test for post request

--- tdd/src/test/java/com/example/tdd/controllers/HomeControllerTests.java
+++ tdd/src/test/java/com/example/tdd/controllers/HomeControllerTests.java
@@ -4,10 +4,12 @@ package com.example.tdd.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(HomeController.class)
@@ -25,5 +27,16 @@ public class HomeControllerTests {
                 .andExpect(view().name("home"))
                 .andExpect(content().string(
                         containsString("Друзья")));
+    }
+
+    @Test
+    void testPostForm() throws Exception {
+        mockMvc.perform(post("/submitForm")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("friendName", "Sergey")
+                        .param("friendLastName", "Sergeev")
+                        .param("friendBirthday", "06-06-2006")
+                )
+                .andExpect(status().isOk());
     }
 }
