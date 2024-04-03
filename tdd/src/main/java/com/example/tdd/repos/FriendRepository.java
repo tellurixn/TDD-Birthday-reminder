424eb5c96fab431506ad4e7a6294e5d8e277dca6
tellurixn <victor.babaryckin@yandex.ru>
2024-04-03 13:10:47 +0700

    GREEN: Created method which finds friends with birthday this month

--- tdd/src/main/java/com/example/tdd/repos/FriendRepository.java
+++ tdd/src/main/java/com/example/tdd/repos/FriendRepository.java
@@ -1,10 +1,14 @@
 package com.example.tdd.repos;
 
 import com.example.tdd.models.Friend;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface FriendRepository extends CrudRepository<Friend, Long> {
     Friend findByLastName(String last_name);
-
+    @Query("SELECT f FROM Friend f WHERE MONTH(f.birthday) = MONTH(CURRENT_DATE) AND DAY(f.birthday) >= DAY(CURRENT_DATE)")
+    List<Friend> findAllFriendsWithBirthdayThisMonth();
 
 }
